e8a0a3721e4378b78aaa23cbc5a5e79a79b2eea9
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2025-09-16 16:52:00 +0700

    feat: adding inheritance and polymorphism

--- constructor/constructor.java
+++ constructor/constructor.java
@@ -1,19 +1,19 @@
 package constructor;
-class constructor {
+class mahasiswa {
     String nama;
     String hobi;
     int umur;
 
-    constructor(String nama, String hobi, int umur) {
+    mahasiswa(String nama, String hobi, int umur) {
         this.nama = nama;
         this.umur = umur;
         this.hobi = hobi;
     }
 }
 
-public class mahasiswa {
+public class constructor {
     public static void main(String[] args) {
-        constructor mhs1 = new constructor("Muhammad Rifky Ramadani", "Mancing", 10);
+        mahasiswa mhs1 = new mahasiswa("Muhammad Rifky Ramadani", "Mancing", 10);
         System.out.println("Halo, nama saya " + mhs1.nama);
         System.out.println("Hobi Saya " + mhs1.hobi);
         System.out.println("Umur Saya " + mhs1.umur + " Tahun");
---
+++ inheritancePolymorphism/inheritancePolymorphism.java
@@ -0,0 +1,45 @@
+package inheritancePolymorphism;
+
+class Dosen {
+    String nama;
+    int umur;
+
+    public Dosen(String nama, int umur) {
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public void mengajar() {
+        System.out.println("Dosen " + nama + " Sedang Mengajar " + "umurnya " + umur);
+    }
+
+    // Static
+    public void mengajar(String mataKuliah) {
+        System.out.println("Dosen dengan nama " + nama + " umurnya " + umur + " sedang mengajar " + mataKuliah);
+    }
+}
+
+class DosenTetap extends Dosen {
+    int gajiPokok;
+
+    public DosenTetap(String nama, int gajiPokok, int umur) {
+        super(nama,umur);
+        this.gajiPokok = gajiPokok;
+    }
+
+    @Override
+    public void mengajar() {
+        System.out.println("Dosen " + nama + " sedang mengajar " + "umurnya " + umur + " gajinya " + gajiPokok);
+    }
+}
+
+public class inheritancePolymorphism {
+    public static void main(String[] args) {
+        // Dynamic
+        Dosen dsn1 = new Dosen("Keisha", 30);
+        dsn1.mengajar();
+
+        // Static
+        dsn1.mengajar("Kompro");
+    }
+}
